cc1d3bb957c6a567a486852529f6fe4b6b5c2d33
huckmon <45248261+huckmon@users.noreply.github.com>
2024-10-31 13:23:55 +0000

    motd fixed and working

---
+++ hosts/anjanath/configuration.nix
@@ -0,0 +1,135 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+      ../../containers/jellyfin.nix
+      ../../containers/deluge.nix
+      ../../containers/nginxproxymanager.nix
+      ../../containers/vaultwarden.nix
+      ../../containers/arr.nix
+      #../../containers/homarr.nix
+      #../../containers/paperless-ngx.nix
+      ./shares/samba.nix
+      ./filesystems/mergerfs.nix
+      ./filesystems/snapraid.nix
+      ../../modules/nixos/powermanagement.nix
+      ../../modules/nixos/syncthing.nix
+      ../../modules/nixos/virtualisation.nix
+    ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "huck" = import ./home.nix;
+    };
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  hardware.opengl.enable = true;	# part of jellyfin
+  hardware.opengl.driSupport = true;	# part of jellyfin
+
+  networking.hostName = "anjanath"; # Define your hostname.
+  networking.firewall.enable = true;
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
+
+  users = {
+    motdFile = "/etc/motd";
+    users.huck = { # move to user file
+      isNormalUser = true;
+      home = "/home/huck";
+      extraGroups = [ "wheel" "share" ];
+      packages = with pkgs; [
+      ];
+    };
+  };
+
+  # List packages installed in system profile. To search, run: $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    docker-compose	#Virtualisation
+    neofetch
+    parted
+    git
+    htop
+    iotop		#debug
+    tree
+    appimage-run
+    ascii-image-converter
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  #systemd timers
+  
+  systemd.timers."mc-backup" = {
+    wantedBy = [ "timers.target" ];
+    partOf = [ "mc-backup.service" ];
+    timerConfig = {
+      OnCalendar = "monthly";
+      Persistent = "true";
+      Unit = "mc-backup.service";
+    };
+  };
+
+  systemd.services."mc-backup" = {
+    #path = with pkgs; [ bash ];
+    script = ''${pkgs.bash}/bin/bash /home/Documents/scripts/mc-backup.sh'';
+    serviceConfig = {
+      Type = "oneshot";
+    };
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+}
---
+++ hosts/anjanath/shares/samba.nix
@@ -0,0 +1,146 @@
+
+{ users, config, pkgs, lib, ... }:
+
+  #samba
+let
+  smb = {
+    share_list = {
+      #Backups = { path = "mnt/user/Backup"; };
+      #Documents = { path = "mnt/user/Documents"; };
+      Media = { path = "mnt/user/Media"; };
+    };
+    share_params = {
+      "browseable" = "yes";
+      "writeable" = "yes";
+      "read only" = "no";
+      "guest ok" = "no";
+      "create mask" = "0644";
+      "directory mask" = "2775";
+      "valid users" = "share";
+    };
+  };
+  smb_shares = builtins.mapAttrs (name: value: value // smb.share_params) smb.share_list;
+in
+{
+services.samba-wsdd.enable = true; # make shares visible for windows 10 clients
+
+
+
+users = {
+groups.share = {
+  gid = 993;
+};
+users.share = {
+  uid = 994;
+  isSystemUser = true;
+  group = "share";
+  };
+};
+
+#users.users.huck.extraGroups = [ "share" ];
+
+#environment.systemPackages = [ config.services.samba.package ];
+
+#environment.systemPackages = with pkgs; [
+#  samba
+#  avahi
+#];
+
+environment = {
+  #systemPackages = [ config.services.samba.package ];
+  systemPackages = with pkgs; [
+    samba
+    avahi
+  ];
+};
+
+systemd.tmpfiles.rules = map (x: "d ${x.path} 0775 share share - -") (lib.attrValues smb.share_list) ++ ["d /mnt 0775 share share - -"];
+#systemd.tmpfiles.rules = [
+   # "d /mnt/user/Backups 0775 share share - -"
+   # "d /mnt/user/Documents 0775 share share - -"
+   # "d /mnt/user/Media 0775 share share - -"
+ # ];
+  services.samba = {
+    enable = true;
+    enableNmbd = true;
+    openFirewall = true;
+    invalidUsers = [ "root" ];
+    securityType = "user";
+    extraConfig = ''
+      workgroup = WORKGROUP
+      server string = serveridiot
+      netbios name = serveridiot
+      security = user 
+      hosts allow = 192.168.60.0/24 192.168.20.0/24
+      guest account = nobody
+      map to guest = bad user
+      '';
+    shares = smb_shares; #{
+#      Backups = { 
+#        path = "/mnt/user/Backups"; 
+#        "browseable" = "yes";
+#        "writeable" = "yes";
+#        "read only" = "no";
+#        "guest ok" = "no";
+#        "create mask" = "0644";
+#        "directory mask" = "0755";
+#        "valid users" = "share";
+#      };
+#      Documents = { 
+#        path = "/mnt/user/Documents";
+#        "browseable" = "yes";
+#        "writeable" = "yes";
+#        "read only" = "no";
+#        "guest ok" = "no";
+#        "create mask" = "0644";
+#        "directory mask" = "0755";
+#        "valid users" = "share";
+#      };
+#      Media = { 
+#        path = "/mnt/user/Media"; 
+#        "browseable" = "yes";
+#        "writeable" = "yes";
+#        "read only" = "no";
+#        "guest ok" = "no";
+#        "create mask" = "0644";
+#        "directory mask" = "0755";
+#        "valid users" = "share";
+#      };
+#      Misc = { 
+#        path = "/mnt/user/Misc"; 
+#        "browseable" = "yes";
+#        "writeable" = "yes";
+#        "read only" = "no";
+#        "guest ok" = "no";
+#        "create mask" = "0644";
+#        "directory mask" = "0755";
+#        "valid users" = "share";
+#      };
+#    };
+  };
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+    extraServiceFiles = {
+      smb = ''
+        <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+        <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+        <service-group>
+        <name replace-wildcards="yes">%h</name>
+        <service>
+        <type>_smb._tcp</type>
+        <port>445</port>
+        </service>
+        </service-group>
+        '';
+    };
+  };
+}
