c9618d5a881da629256e4d655ca8ef06c3a3df38
Huck <45248261+huckmon@users.noreply.github.com>
2025-04-30 20:56:28 +0930

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs-normal.url = "github:nixos/nixpkgs/nixos-unstable"
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     home-manager = {
