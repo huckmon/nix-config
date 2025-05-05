b886539c082986aa2c014c4fb04f425fda803c79
huckmon <45248261+huckmon@users.noreply.github.com>
2025-05-05 15:29:13 +0930

    Readded hostnames

--- hosts/greatjaggi/default.nix
+++ hosts/greatjaggi/default.nix
@@ -60,7 +60,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
 
-  networking.hostName = ${hostname}; # Define your hostname.
+  networking.hostName = "greatjaggi"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
