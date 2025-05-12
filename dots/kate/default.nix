0e0ff8d89cf4fa2906db9f0d64035edc7b6e618c
Huck <45248261+huckmon@users.noreply.github.com>
2025-05-12 18:15:12 +0930

    Added, config for kate with language servers

---
+++ dots/kate/default.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, inputs, ... }:
+
+{
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.kate
+    nil
+    dockerfile-language-server-nodejs
+    bash-language-server
+    python-lsp-server
+  ];
+
+}
