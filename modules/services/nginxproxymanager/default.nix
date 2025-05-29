ce173090ce3953a3bc42f27a9ec3acd5c6bb5b2c
Huck <45248261+huckmon@users.noreply.github.com>
2025-05-29 22:04:33 +0930

    Fix, indenting and forced container to work on host network

--- modules/services/nginxproxymanager/default.nix
+++ modules/services/nginxproxymanager/default.nix
@@ -15,18 +15,19 @@ in
       containers = {
         nginxproxymanager = {
           image = "jc21/nginx-proxy-manager:latest";
-      autoStart = true;
-      volumes = [
-        "/srv/nginx/data:/data"
-        "/srv/nginx/letsencrypt:/etc/letsencrypt"
-  #	  "${vars.serviceConfigDir}/nginx/data:/data"
-  #	  "${vars.serviceConfigDir}/nginx/letsencrypt:/etc/letsencrypt"
-      ];
-      ports = [
-        "80:80"
-        "81:81"
-        "443:443"
-      ];
+          autoStart = true;
+          volumes = [
+            "/srv/nginx/data:/data"
+            "/srv/nginx/letsencrypt:/etc/letsencrypt"
+      #	  "${vars.serviceConfigDir}/nginx/data:/data"
+      #	  "${vars.serviceConfigDir}/nginx/letsencrypt:/etc/letsencrypt"
+          ];
+          ports = [
+            "80:80"
+            "81:81"
+            "443:443"
+          ];
+          extraOptions = [ "--network=host" ];
         };
       };
     };
