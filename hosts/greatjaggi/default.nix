62366e1f72bf3f543d550286ce39773e47bb5a43
huckmon <45248261+huckmon@users.noreply.github.com>
2025-06-08 23:24:44 +0930

    fix, automaount shouldn't be on samba drive options

--- hosts/greatjaggi/default.nix
+++ hosts/greatjaggi/default.nix
@@ -49,9 +49,7 @@
     device = "//192.168.60.219/Media";
     fsType = "cifs";
     options = let
-
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
-
+      automount_opts = "noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
     in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1001,gid=100"];
   };
 
