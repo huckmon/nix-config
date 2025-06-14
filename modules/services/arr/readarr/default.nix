c0eed3c8a7c1df16bd1e7d7745f369644d424c3e
huckmon <45248261+huckmon@users.noreply.github.com>
2025-06-14 15:57:56 +0930

    Add, readar config

---
+++ modules/services/arr/readarr/default.nix
@@ -0,0 +1,22 @@
+{ config, vars, lib, ... }:
+let
+  service = "readarr";
+  cfg = config.customModules.services.${service};
+in
+{
+  options.customModules.services.${service} = {
+    enable = lib.mkEnableOption "Enable ${service} service";
+    configDir = lib.mkOption {
+      default = "var/lib/${service}";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.${service} = {
+      enable = true;
+      user = config.customModules.user;
+      group = config.customModules.group;
+      openFirewall = true;
+    };
+  };
+}
