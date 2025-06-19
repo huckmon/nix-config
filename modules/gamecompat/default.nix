cf7616bdf25b9007d2cebddc39557fa66d3be0c6
huckmon <45248261+huckmon@users.noreply.github.com>
2025-06-19 15:40:10 +0930

    Add, gamecompat module with wine, protontricks, etc

---
+++ modules/gamecompat/default.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, lib, ... }:
+let
+    cfg = config.customModules;
+in
+{
+  options.customModules.gameCompat = {
+    enable = lib.mkEnableOption "Enables gameCompat module for wine, wintricks, etc";
+  };
+
+  config = lib.mkIf cfg.gameCompat.enable {
+
+    environment.systemPackages = with pkgs; [
+        winetricks
+        protontricks
+        wineWowPackages.stable
+        lutris
+    ];
+
+  };
+
+}
